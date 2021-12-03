6333605441f4597dd45157683cb13a50d79bf545
yeefea <yifei.shen@yahoo.com>
2021-12-04 02:08:56 +0800

    algo

--- c_workspace/leetcode/10.cpp
+++ c_workspace/leetcode/10.cpp
@@ -18,11 +18,48 @@ public:
         {
             return false;
         }
+
         vector<vector<int>> dp(s_size + 1, vector<int>(p_size + 1));
         dp[0][0] = true;
-        
+        // 最左边一列总是false
+
+        for (int i = 0; i <= s_size; ++i)
+        {
+            for (int j = 1; j <= p_size; ++j)
+            {
+                if (p[j - 1] == '*')
+                {
+                    dp[i][j] |= dp[i][j - 2];
+                    if (matches(s, p, dp, i, j - 1))
+                    {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+                else
+                {
+                    if (matches(s, p, dp, i, j))
+                    {
+                        dp[i][j] |= dp[i - 1][j - 1];
+                    }
+                }
+            }
+        }
 
         return dp[s_size][p_size];
+    }
+
+    bool matches(string &s, string &p, vector<vector<int>> &dp, int i, int j)
+    {
+        // 关键函数
+        if (i == 0)
+        {
+            return false;
+        }
+        if (p[j - 1] == '.')
+        {
+            return true;
+        }
+        return s[i - 1] == p[j - 1];
     }
 };
 
